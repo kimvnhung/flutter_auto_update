1be73838e9197062c3de3578203edd411037a9db
kimvnhung <kimvnhung@gmail.com>
2023-12-04 23:30:21 +0700

    comapre version, make version greater to update

--- android/src/main/java/com/example/flutter_auto_update/Github.java
+++ android/src/main/java/com/example/flutter_auto_update/Github.java
@@ -12,7 +12,7 @@ import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
 
-public class Github extends Thread{
+public class Github extends Thread {
     private static final String TAG = Github.class.getName();
     private static final String url = "https://api.github.com/repos/%s/%s/releases/latest";
 
@@ -28,27 +28,27 @@ public class Github extends Thread{
     public int fetched = 0;
 
     public Github(
-            String userName,String githubToken, String packageName, String type, String fileName, String versionName
-    ){
+            String userName, String githubToken, String packageName, String type, String fileName, String versionName) {
         this.userName = userName;
         this.githubToken = githubToken;
         this.packageName = packageName;
         this.type = type;
         this.fileName = fileName;
         this.versionCode = versionName;
-        Log.d(TAG,"Github: "+userName+" "+githubToken+" "+packageName+" "+type+" "+fileName+" "+versionName);
+        Log.d(TAG, "Github: " + userName + " " + githubToken + " " + packageName + " " + type + " " + fileName + " "
+                + versionName);
     }
 
     @Override
-    public void run(){
+    public void run() {
         try {
             URL requestUrl = new URL(String.format(url, userName, packageName));
-            Log.d(TAG,"resquestUrl "+requestUrl.toString());
+            Log.d(TAG, "resquestUrl " + requestUrl.toString());
             HttpsURLConnection connection = (HttpsURLConnection) requestUrl.openConnection();
             connection.setRequestProperty("User-Agent", "auto-update");
-            
+
             if (githubToken != null && !githubToken.isEmpty()) {
-                connection.setRequestProperty("Authorization", "Bearer "+githubToken);
+                connection.setRequestProperty("Authorization", "Bearer " + githubToken);
             }
 
             if (connection.getResponseCode() == 200) {
@@ -58,42 +58,57 @@ public class Github extends Thread{
                     StringBuffer sb = new StringBuffer();
                     char[] chunk = new char[FileDownloader.chunkSize];
                     int read = 0;
-                    while((read = inputStream.read(chunk)) > 0){
+                    while ((read = inputStream.read(chunk)) > 0) {
                         sb.append(chunk, 0, read);
                     }
 
                     String jsonData = sb.toString();
                     JSONObject jsonObject = new JSONObject(jsonData);
-                    Log.d(TAG,"tag_name "+jsonObject.get("tag_name")+" "+versionCode);
+                    Log.d(TAG, "tag_name " + jsonObject.get("tag_name") + " " + versionCode);
                     if (!jsonObject.get("tag_name").equals(versionCode)) {
-                        JSONArray assets = (JSONArray) jsonObject.get("assets");
-                        for (int i = 0; i < assets.length(); i++) {
-                            JSONObject asset = assets.getJSONObject(i);
-                            if (
-                                    asset.get("content_type").toString().equals(type) &&
-                                    asset.get("name").toString().equals(fileName)
-                            ) {
-                                githubResults = new GithubResults(
-                                    // asset.get("browser_download_url").toString(),
-                                    asset.get("url").toString(),
-                                    jsonObject.get("body").toString(),
-                                    jsonObject.get("tag_name").toString()
-                                );
-                                fetched = 1;
+                        String[] versionCodes = versionCode.split(".");
+                        String[] tagNames = jsonObject.getString("tag_name").split(".");
+                        boolean isUpdatable = false;
+                        for (int i = 0; i < tagNames.length; i++) {
+                            int targetVer = Integer.parseInt(tagNames[i]);
+                            int curVer = Integer.parseInt(versionCodes[i]);
+                            if (targetVer != curVer) {
+                                if (targetVer > curVer) {
+                                    isUpdatable = true;
+                                }
                                 break;
                             }
                         }
-                    } else {
+                        if (isUpdatable) {
+                            JSONArray assets = (JSONArray) jsonObject.get("assets");
+                            for (int i = 0; i < assets.length(); i++) {
+                                JSONObject asset = assets.getJSONObject(i);
+                                if (asset.get("content_type").toString().equals(type) &&
+                                        asset.get("name").toString().equals(fileName)) {
+                                    githubResults = new GithubResults(
+                                            // asset.get("browser_download_url").toString(),
+                                            asset.get("url").toString(),
+                                            jsonObject.get("body").toString(),
+                                            jsonObject.get("tag_name").toString());
+                                    fetched = 1;
+                                    break;
+                                }
+                            }
+                        }else{
+                            fetched = 2;
+                        }
+                    }
+                    else {
                         fetched = 2;
                     }
                 } finally {
                     connection.disconnect();
                 }
             }
-            Log.d(TAG,"response code "+connection.getResponseCode());
-            Log.d(TAG,"response message "+connection.getResponseMessage());
+            Log.d(TAG, "response code " + connection.getResponseCode());
+            Log.d(TAG, "response message " + connection.getResponseMessage());
         } catch (IOException | JSONException e) {
-            Log.e(TAG,e.getMessage());
+            Log.e(TAG, e.getMessage());
             exception = e;
             fetched = -1;
         }
